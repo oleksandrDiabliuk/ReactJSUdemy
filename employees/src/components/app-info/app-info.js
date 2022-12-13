39090fe17084c77ff7da1c221a5be65dcc2b7679
odiabliuk <o.diabliuk@gmail.com>
2022-12-13 15:50:50 +0200

    app info functionality

---
+++ employees/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = ({employeeCount, employeeIncreaseCount}) => {
+    return (
+        <div className="app-info">
+            <h1>Employee Accounting in company Unknown</h1>
+            <h2>General employee count: {employeeCount}</h2>
+            <h2>Premium will receive: {employeeIncreaseCount}</h2>
+        </div>
+    );
+}
+
+export default AppInfo;
