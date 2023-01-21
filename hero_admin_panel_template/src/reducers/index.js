deece4125bfafefd166fc8bc5f0a250a21585782
odiabliuk <o.diabliuk@gmail.com>
2023-01-21 19:54:59 +0200

    test project init

---
+++ hero_admin_panel_template/src/reducers/index.js
@@ -0,0 +1,36 @@
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: 'idle',
+    filters: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'HEROES_FETCHING':
+            return {
+                ...state,
+                heroesLoadingStatus: 'loading'
+            }
+        case 'HEROES_FETCHED':
+            return {
+                ...state,
+                heroes: action.payload,
+                heroesLoadingStatus: 'idle'
+            }
+        case 'HEROES_FETCHING_ERROR':
+            return {
+                ...state,
+                heroesLoadingStatus: 'error'
+            }
+        // case 'HERO_DELETED':
+        //     console.log(action.payload);    
+
+        //     return {
+        //         ...state,
+        //         heroesLoadingStatus: 'error'
+        //     }
+        default: return state
+    }
+}
+
+export default reducer;
