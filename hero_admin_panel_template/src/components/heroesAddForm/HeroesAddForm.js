358262cd205d43ad1988782bec086f9d34327e57
odiabliuk <o.diabliuk@gmail.com>
2023-01-24 22:46:10 +0200

    fix issue with filters element in heroesAddForm

--- hero_admin_panel_template/src/components/heroesAddForm/HeroesAddForm.js
+++ hero_admin_panel_template/src/components/heroesAddForm/HeroesAddForm.js
@@ -2,7 +2,9 @@ import {useHttp} from '../../hooks/http.hook';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
+import store from '../../store';
 
+import { selectAll } from '../heroesFilters/filtersSlice';
 import { heroCreate } from '../heroesList/heroesSlice';
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
@@ -19,7 +21,8 @@ const HeroesAddForm = () => {
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
 
-    const {filters, filterLoadingStatus} = useSelector(state => state.filters);
+    const {filterLoadingStatus} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
     const dispatch = useDispatch();
     const {request} = useHttp();
 
